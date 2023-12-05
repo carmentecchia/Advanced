0711f72dd42237214bac15a9179743bc9b64ae08
carmentecchia <carmentecchia@gmail.com>
2023-12-05 17:43:46 +0100

    .

---
+++ src/TernaryOp1/TernOp.java
@@ -0,0 +1,30 @@
+package TernaryOp1;
+
+//public class Module01 {
+//    public static void main(String[] args) {
+//        System.out.println(checkString("Questa è una stringa"));
+//    }
+//
+//    private static String checkString(String value) {
+//        String result;
+//        if (value.length() >= 10) {
+//            result = "Lunghezza maggiore o uguale di 10";
+//        } else {
+//            result = "Lunghezza minore di 10";
+//        }
+//        return result;
+//    }
+//}
+public class TernOp {
+
+        public static void main(String[] args) {
+            System.out.println(checkString("Questa è una stringa"));
+        }
+
+        private static String checkString(String value) {
+            String result = (value.length() >= 10) ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+            return result;
+        }
+
+
+}

10cfb7e7828e39ca791d34fce24c405103559bdf
carmentecchia <carmentecchia@gmail.com>
2023-12-05 19:17:12 +0100

    .

--- src/Test3/Main.java
+++ src/Test3/Main.java
@@ -8,8 +8,15 @@ public class Main {
     public static void main(String[] args) {
         OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
 
-        String dataStringMedium = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        mediumFormat(date);
+        }
 
-            System.out.println("Medium:" + dataStringMedium);
+        public static String mediumFormat(OffsetDateTime date) {
+
+        String dataStringMedium = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM));
+        System.out.println("Medium:" + dataStringMedium);
+        return dataStringMedium;
+
+
         }
 }
--- src/Test3/MainTest.java
+++ src/Test3/MainTest.java
@@ -1,22 +1,20 @@
 package Test3;
 
-import Test2.Main;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
     private final Main testing = new Main();
+    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
     @Test
     void testFormat() {
-        String input = "2023-03-01T13:00:00Z";
-        OffsetDateTime odt = OffsetDateTime.parse(input);
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd MMMM yyyy");
-        String dataFormattata = format.format(odt);
-        assertEquals("01 marzo 2023", dataFormattata);
+        String outputFormatted = testing.mediumFormat(date);
+        assertEquals("1 mar 2023", outputFormatted);
 
     }
 

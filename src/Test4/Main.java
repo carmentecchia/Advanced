12a6f3b51ff6378f15c6e43bbba24f68fffe3d46
carmentecchia <carmentecchia@gmail.com>
2023-12-05 20:01:08 +0100

    .

--- src/Test4/Main.java
+++ src/Test4/Main.java
@@ -1,19 +1,36 @@
 package Test4;
 
+import java.time.DayOfWeek;
+import java.time.Month;
+import java.time.OffsetDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        String dateString = "2023-03-01T13:00:00Z";
-        java.time.OffsetDateTime offsetDateTime = java.time.OffsetDateTime.parse(dateString);
 
-        int year = offsetDateTime.getYear();
-        int month = offsetDateTime.getMonthValue();
-        int day = offsetDateTime.getDayOfMonth();
-
-        String dayOfWeek = offsetDateTime.getDayOfWeek().toString();
-
-        System.out.println("Anno: " + year);
-        System.out.println("Mese: " + month);
-        System.out.println("Giorno: " + day);
-        System.out.println("Giorno della settimana: " + dayOfWeek);
+        OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+        getYear(date);
+        getMonth(date);
+        getDay(date);
+        getDayOfWeek(date);
+    }
+    public static int getYear(OffsetDateTime date){
+        int dataYear = date.getYear();
+        System.out.println("Anno: " + dataYear);
+        return dataYear;
+    }
+    public static Month getMonth(OffsetDateTime date){
+        Month dataMonth = date.getMonth();
+        System.out.println("Mese: " + dataMonth);
+        return dataMonth;
+    }
+    public static int getDay(OffsetDateTime date){
+        int dataDay = date.getDayOfMonth();
+        System.out.println("Giorno: " + dataDay);
+        return dataDay;
+    }
+    public static DayOfWeek getDayOfWeek(OffsetDateTime date){
+        DayOfWeek dataDayOfWeek = date.getDayOfWeek();
+        System.out.println("Giorno della settimana: " + dataDayOfWeek);
+        return dataDayOfWeek;
     }
 }
--- src/Test4/MainTest.java
+++ src/Test4/MainTest.java
@@ -1,20 +1,39 @@
 package Test4;
+import org.junit.jupiter.api.Test;
 
-import Test2.Main;
-import org.junit.Test;
-
+import java.time.DayOfWeek;
+import java.time.Month;
 import java.time.OffsetDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static java.time.DayOfWeek.WEDNESDAY;
+import static java.time.Month.MARCH;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class MainTest {
-    private OffsetDateTime dateTime;
+
     private final Main testing = new Main();
+    OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
     @Test
-    public void main() {
-        int expected = 2023;
-        int actual = 2022;
-        assertEquals (expected, actual);
+    void getYearTest () {
+        int result = testing.getYear(date);
+        assertEquals(2023,result);
+    }
+    @Test
+    void getMonthTest() {
+        Month result = testing.getMonth(date);
+        assertEquals(MARCH,result);
     }
 
+    @Test
+    void getDayTest() {
+        int result = testing.getDay(date);
+        assertEquals(1,result);
+    }
+
+    @Test
+    void getDayOfWeekTest() {
+        DayOfWeek result = testing.getDayOfWeek(date);
+        assertEquals(WEDNESDAY,result);
+    }
 }

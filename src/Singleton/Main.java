de776d005f23053055e50070057a3760fec5949c
carmentecchia <carmentecchia@gmail.com>
2023-12-09 15:46:40 +0100

    .

--- src/Singleton/Main.java
+++ src/Singleton/Main.java
@@ -1,6 +1,15 @@
 package Singleton;
 
 public class Main {
+
+    private static User user;
+    public static User getUser(){
+        if(user == null){
+            user = new User();
+        }
+        return user;
+    }
+
     public static void main(String[] args) {
         User user1 = new User();
         User user2 = new User();
